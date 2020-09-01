59782b394e01cafcbb9e3719685cc0e082022d63
ds-praveenkumar <praveenkr2208@gmail.com>
2020-09-02 00:37:22 +0530

    add transformer module

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # timeseries-analysis
 data analysis related to Time series forecasting and prediction
+
+NOTE:
+`data` folder needs to be added which must contain all the columns ['Date', 'Open', 'High', 'Low', 'Close', 'Shares Traded', 'Turnover (Rs. Cr)']
--- bank_nifty_analysis/main.py
+++ bank_nifty_analysis/main.py
@@ -2,19 +2,51 @@
 
 import pandas as pd
 from pathlib import Path
+from sklearn.preprocessing import MinMaxScaler
 
 
-# read data
-DATA_PATH = Path(Path().cwd()).resolve() / "data"
-FILES = list(DATA_PATH.glob("*.csv"))
+class BankNiftyPrediction():
 
-print(f'total files: {len(FILES)}')
-for file in FILES:
-    df = pd.DataFrame()
-    df_ = pd.read_csv(str(file))
-    df = df.append(df_)
+    __slots__ = ["path", "df"]
 
-print(df.columns)
+    def __init__(self, path):
+        self.path  = path
+        self.df = pd.DataFrame()
+    
+
+    def read_csv(self):
+
+        FILES = list(self.path.glob("*.csv"))
+        print(f'total files found: {len(FILES)}')
+        for file in FILES:
+            df_ = pd.read_csv(str(file))
+            self.df =  self.df.append(df_)
+        return self.df
+    
+
+
+    def scale_data(self, col_to_transform):
+        self.df = self.read_csv()
+        close_ndarray = self.df[col_to_transform].values.reshape((-1,1))
+        mn = MinMaxScaler(feature_range=(0, 1))
+        self.df["transformed_feature"] = mn.fit_transform(close_ndarray) 
+        print(self.df.transformed_feature[:5])
+        return self.df, mn
+
+    def main(self ):
+        pass
+        
+
+if __name__ == "__main__":
+    # read data
+    DATA_PATH = Path(Path().cwd()).resolve() / "data"
+    bkobj = BankNiftyPrediction(DATA_PATH)
+    bkobj.scale_data(col_to_transform="Close")
+    
+
+
+
+
 
 
 
