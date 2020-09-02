02e49ef83ad741f9ca5dc4c7857b615ceac47b5e
ds-praveenkumar <praveenkr2208@gmail.com>
2020-09-02 18:22:10 +0530

    added class methods and training data prepration module

--- bank_nifty_analysis/main.py
+++ bank_nifty_analysis/main.py
@@ -7,41 +7,59 @@ from sklearn.preprocessing import MinMaxScaler
 
 class BankNiftyPrediction():
 
-    __slots__ = ["path", "df"]
+    __slots__ = ["path", "df", "features", "target_col"]
 
-    def __init__(self, path):
+    def __init__(self, path, features, target_col):
         self.path  = path
         self.df = pd.DataFrame()
-    
-
-    def read_csv(self):
-
+        self.features = features
+        self.target_col = target_col
         FILES = list(self.path.glob("*.csv"))
         print(f'total files found: {len(FILES)}')
         for file in FILES:
             df_ = pd.read_csv(str(file))
             self.df =  self.df.append(df_)
+
+
+    def generate_features(self):
+
+        for lag in range(self.features):
+            self.df["lag"+str(lag+1)] = self.df[self.target_col].shift(lag+1)
+        print(self.df.columns)
+        self.df = self.df.dropna()
         return self.df
-    
 
 
-    def scale_data(self, col_to_transform):
-        self.df = self.read_csv()
-        close_ndarray = self.df[col_to_transform].values.reshape((-1,1))
+    def scale_data(self):
+
+        df = self.df
+        close_ndarray = df[self.target_col].values.reshape((-1,1))
         mn = MinMaxScaler(feature_range=(0, 1))
         self.df["transformed_feature"] = mn.fit_transform(close_ndarray) 
         print(self.df.transformed_feature[:5])
         return self.df, mn
-
-    def main(self ):
-        pass
         
+    
+    def prepare_training_data(self):
+
+        features_array = self.df.iloc[:, 7:].values
+        target = self.df[self.target_col].values
+        print(features_array[:5])
+        return target, features_array
+
+    @classmethod
+    def main(cls):
+        DATA_PATH = Path(Path().cwd()).resolve() / "data"
+        features=5
+        target_col="Close"
+        bkobj = BankNiftyPrediction(path=DATA_PATH, features=features, target_col=target_col )
+        bkobj.generate_features()
+        bkobj.scale_data()
+        bkobj.prepare_training_data()
 
 if __name__ == "__main__":
-    # read data
-    DATA_PATH = Path(Path().cwd()).resolve() / "data"
-    bkobj = BankNiftyPrediction(DATA_PATH)
-    bkobj.scale_data(col_to_transform="Close")
+    BankNiftyPrediction.main()
+    
     
 
 
