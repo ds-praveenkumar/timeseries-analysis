ac1b052b3f5b0e67cd1c8ac95164ac85849dcaf6
ds-praveenkumar <praveenkr2208@gmail.com>
2020-09-06 04:48:12 +0530

    added train val error plot

--- bank_nifty_analysis/build_model.py
+++ bank_nifty_analysis/build_model.py
@@ -1,9 +1,11 @@
 # build_model.py
 from pathlib import Path
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd 
 import tensorflow as tf 
 from tensorflow.keras.layers import Dense, Dropout, LSTM 
+from tensorflow.keras.callbacks import ModelCheckpoint
 import numpy as np
 
 class LSTMModel():
@@ -16,6 +18,9 @@ class LSTMModel():
         self.model = tf.keras.Sequential()
         self.features = None
         self.target = None
+        self.history = None
+        self.x_test = None
+        self.y_test = None
 
 
     def read_processed_data(self):
@@ -23,12 +28,14 @@ class LSTMModel():
         path = Path(Path().cwd()).resolve()
         files = list(path.glob("*.npy"))
         features = np.load(str(files[0]))
-        train_len = int(len(features) * 0.70)
+        train_len = int(len(features) * 0.75)
         train_set = features[:train_len]
         test_set = features[train_len:]
         target = np.load(str(files[1]))
         target_train = target[:train_len]
         target_test = target[train_len:]
+        self.x_test = test_set
+        self.y_test = target_test
         self.features = train_set.reshape((train_set.shape[0], 1, train_set.shape[1]))
         self.target = target_train.reshape((-1,1))
         self.input_shape = features.shape
@@ -54,18 +61,53 @@ class LSTMModel():
         self.model.add(Dense(10, activation="relu"))
         self.model.add(Dense(units=self.output_shape ))
 
-        self.model.compile(optimizer='adam', loss='mape', metrics=['mape', 'accuracy'])
+        self.model.compile(optimizer='adam', loss='mape', metrics=['mape'])
         self.model.save("Nifty_bank_model.h5")
         print(self.model.summary(), end='\n')
 
     
     def train(self, epochs):
 
-        self.model = self.model.fit(x=self.features, 
+        checkpoint = ModelCheckpoint(filepath="./model", 
+                                    save_best_only=True,
+                                    save_weights_only=False,
+                                    mode='auto')
+
+        self.history = self.model.fit(x=self.features, 
                                     y=self.target,
                                     epochs=epochs,
                                     verbose=True,
-                                    batch_size=32)
+                                    batch_size=32,
+                                    callbacks=[checkpoint])
+        return self.history
+
+    def evaluate(self,
+                 y,
+                 x=range(30),
+                 title= "training loss per epoch",
+                 xlabel="epochs",
+                 ylabel="loss",
+                 file="evalute.png",
+                 ):
+
+        
+        print("showing plot")
+        plot = sns.lineplot( y=y, x=x)
+        # plot = sns.lineplot( y=self.history.history["val_loss"], x=range(30))
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        fig = plot.get_figure()
+        fig.savefig(file)
+        plt.show()
+
+    
+    def predict(self):
+
+        predictions = self.model.predict(self.x_test)
+        self.evaluate(y=predictions, file="train_val.png")
+
+        
         
 
     @classmethod
@@ -74,7 +116,9 @@ class LSTMModel():
         lstm = LSTMModel(lstm_units=256, output_shape=1)
         lstm.read_processed_data()
         lstm.build_model()
-        lstm.train(epochs=70)
+        history = lstm.train(epochs=30)
+        lstm.evaluate(y=history.history["loss"])
+        #lstm.predict()
 
 
 if __name__ == '__main__':
